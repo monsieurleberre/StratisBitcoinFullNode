868ac9a558ee7b4cb43085bcd9c7b64d51868631
Matthieu Le Berre <monsieur.leberre@gmail.com>
2018-10-02 17:23:10 +0100

    Extract code from SmartContractsNetwork ang to Smash

---
+++ src/Stratis.Bitcoin.Networks/Smash/Constants.cs
@@ -0,0 +1,18 @@
+﻿namespace Stratis.Bitcoin.Networks.Smash
+{
+    public class Constants
+    {
+        /// <summary> Smash maximal value for the calculated time offset. If the value is over this limit, the time syncing feature will be switched off. </summary>
+        public const int MaxTimeOffsetSeconds = 25 * 60;
+
+        /// <summary> Smash default value for the maximum tip age in seconds to consider the node in initial block download (2 hours). </summary>
+        public const int DefaultMaxTipAgeInSeconds = 2 * 60 * 60;
+
+        /// <summary> The name of the root folder containing the different Smash blockchains (SmashMain, SmashTest, SmashRegTest). </summary>
+        public const string RootFolderName = "smash";
+
+        /// <summary> The default name used for the Smash configuration file. </summary>
+        public const string DefaultConfigFilename = "smash.conf";
+
+    }
+}
---
+++ src/Stratis.Bitcoin.Networks/Smash/SmashTest.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using NBitcoin;
+using NBitcoin.DataEncoders;
+using NBitcoin.Protocol;
+using Stratis.Bitcoin.Networks;
+
+namespace Stratis.Bitcoin.Features.SmartContracts.Networks
+{
+    public abstract class SmashTest : Network
+    {
+        private const int pubKeyAddressByte = 43; //J
+        private const int scriptAddressByte = 105; //j
+
+        protected SmashTest(ConsensusFactory consensusFactory, Action<Block> customiseGenesisBlock)
+        {
+            this.Name = nameof(SmashTest);
+            this.RootFolderName = StratisMain.StratisRootFolderName;
+            this.DefaultConfigFilename = StratisMain.StratisDefaultConfigFilename;
+            this.Magic = 0x0709110E; // Incremented 19/06
+            this.DefaultPort = 18333;
+            this.RPCPort = 18332;
+            this.MaxTipAge = BitcoinMain.BitcoinDefaultMaxTipAgeInSeconds;
+            this.MinTxFee = 1000;
+            this.FallbackFee = 20000;
+            this.MinRelayTxFee = 1000;
+            this.MaxTimeOffsetSeconds = 25 * 60;
+            
+            Block genesisBlock = GenesisBuilder.CreateBitcoinGenesisBlock(consensusFactory, 1296688602, 414098458, 0x1d00ffff, 1, Money.Coins(50m), customiseGenesisBlock);
+            genesisBlock.Header.Nonce = 3; // Incremented 19/06
+
+            this.Genesis = genesisBlock;
+
+            // Taken from StratisX.
+            var consensusOptions = new PosConsensusOptions(
+                maxBlockBaseSize: 1_000_000,
+                maxStandardVersion: 2,
+                maxStandardTxWeight: 100_000,
+                maxBlockSigopsCost: 20_000,
+                provenHeadersActivationHeight: 10_000_000 // TODO: Set it to the real value once it is known.
+            );
+
+            var buriedDeployments = new BuriedDeploymentsArray
+            {
+                [BuriedDeployments.BIP34] = 21111,
+                [BuriedDeployments.BIP65] = 581885,
+                [BuriedDeployments.BIP66] = 330776
+            };
+
+            var bip9Deployments = new BIP9DeploymentsArray
+            {
+                [BIP9Deployments.TestDummy] = new BIP9DeploymentsParameters(28, 1199145601, 1230767999),
+                [BIP9Deployments.CSV] = new BIP9DeploymentsParameters(0, 1456790400, 1493596800),
+                [BIP9Deployments.Segwit] = new BIP9DeploymentsParameters(1, 1462060800, 1493596800)
+            };
+
+            this.Consensus = new NBitcoin.Consensus(
+                consensusFactory: consensusFactory,
+                consensusOptions: consensusOptions,
+                coinType: 1,
+                hashGenesisBlock: genesisBlock.Header.GetHash(),
+                subsidyHalvingInterval: 210000,
+                majorityEnforceBlockUpgrade: 51,
+                majorityRejectBlockOutdated: 75,
+                majorityWindow: 100,
+                buriedDeployments: buriedDeployments,
+                bip9Deployments: bip9Deployments,
+                bip34Hash: new uint256("0x0000000023b3a96d3484e5abb3755c413e7d41500f8e2a5c3f0dd01299cd8ef8"),
+                ruleChangeActivationThreshold: 1512, // 75% for testchains
+                minerConfirmationWindow: 2016, // nPowTargetTimespan / nPowTargetSpacing
+                maxReorgLength: 500,
+                defaultAssumeValid: new uint256("0x000000003ccfe92231efee04df6621e7bb3f7f513588054e19f78d626b951f59"), // 1235126
+                maxMoney: long.MaxValue,
+                coinbaseMaturity: 5,
+                premineHeight: 2,
+                premineReward: Money.Coins(1000000),
+                proofOfWorkReward: Money.Coins(50),
+                powTargetTimespan: TimeSpan.FromDays(14), // two weeks
+                powTargetSpacing: TimeSpan.FromSeconds(20), // 20 second block time while on testnet
+                powAllowMinDifficultyBlocks: true,
+                powNoRetargeting: false,
+                powLimit: new Target(new uint256("7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")), // Set extremely low difficulty for now.
+                minimumChainWork: uint256.Zero,
+                isProofOfStake: default(bool),
+                lastPowBlock: default(int),
+                proofOfStakeLimit: null,
+                proofOfStakeLimitV2: null,
+                proofOfStakeReward: Money.Zero
+            );
+
+            this.Base58Prefixes = new byte[12][];
+            this.Base58Prefixes[(int)Base58Type.PUBKEY_ADDRESS] = new byte[] { pubKeyAddressByte };
+            this.Base58Prefixes[(int)Base58Type.SCRIPT_ADDRESS] = new byte[] { (scriptAddressByte) };
+            this.Base58Prefixes[(int)Base58Type.SECRET_KEY] = new byte[] { (pubKeyAddressByte + 128) };
+            this.Base58Prefixes[(int)Base58Type.ENCRYPTED_SECRET_KEY_NO_EC] = new byte[] { 0x01, 0x42 };
+            this.Base58Prefixes[(int)Base58Type.ENCRYPTED_SECRET_KEY_EC] = new byte[] { 0x01, 0x43 };
+            this.Base58Prefixes[(int)Base58Type.EXT_PUBLIC_KEY] = new byte[] { (0x04), (0x35), (0x87), (0xCF) };
+            this.Base58Prefixes[(int)Base58Type.EXT_SECRET_KEY] = new byte[] { (0x04), (0x35), (0x83), (0x94) };
+            this.Base58Prefixes[(int)Base58Type.PASSPHRASE_CODE] = new byte[] { 0x2C, 0xE9, 0xB3, 0xE1, 0xFF, 0x39, 0xE2 };
+            this.Base58Prefixes[(int)Base58Type.CONFIRMATION_CODE] = new byte[] { 0x64, 0x3B, 0xF6, 0xA8, 0x9A };
+            this.Base58Prefixes[(int)Base58Type.STEALTH_ADDRESS] = new byte[] { 0x2b };
+            this.Base58Prefixes[(int)Base58Type.ASSET_ID] = new byte[] { 115 };
+            this.Base58Prefixes[(int)Base58Type.COLORED_ADDRESS] = new byte[] { 0x13 };
+
+            Bech32Encoder encoder = Encoders.Bech32("tb");
+            this.Bech32Encoders = new Bech32Encoder[2];
+            this.Bech32Encoders[(int)Bech32Type.WITNESS_PUBKEY_ADDRESS] = encoder;
+            this.Bech32Encoders[(int)Bech32Type.WITNESS_SCRIPT_ADDRESS] = encoder;
+
+            this.Checkpoints = new Dictionary<int, CheckpointInfo>();
+
+            this.DNSSeeds = new List<DNSSeedData>();
+            this.SeedNodes = new List<NetworkAddress>();
+        }
+    }
+}
